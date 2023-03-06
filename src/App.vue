df7d170ed77493f1f9f52628e0f57b2a8db3d6d6
osmn-byhn <osmanbeyhan12@gmail.com>
2023-03-06 04:22:28 +0300

    Primary Commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
 <script setup>
+  import Navbar from './components/Navbar.vue';
+  import { reactive } from "vue";
+  const todo = reactive({
+    date: Date.now,
+    text: '',
+    isCompleted: false
+  })
+  function addTodo() {
+    localStorage.setItem("mission", todo.text)
+  }
+  
+  
 </script>
 
 <template>
-  <header>
-    <h1>NoteApp</h1>
-  </header>
+  <Navbar />
+  <div class="main">
+    <progress max="1" value="0"></progress>
+    <div>Clear completed todos</div>
+    <hr>
+    <div class="form">
+      <form>
+        <input type="text" name="" v-model="todo.text">
+        <button @click="addTodo()">Add</button>
+      </form>
+    </div>
+  </div>
+  <div>{{ note }}</div>
 </template>
 
 <style scoped lang="scss">
---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<script setup>
+    const toggle = () => {
+        var icon = document.getElementById('icon')
+        if(document.body.classList.contains("light")){
+            document.body.classList.remove("light")
+            icon.setAttribute('class', 'bi bi-sun-fill')
+        } else {
+            document.body.classList.add("light")
+            icon.setAttribute('class', 'bi bi-moon-fill')
+        }
+    }
+</script>
+
+<template>
+    <nav>
+        <ul>
+            <li>
+                <h1>Notes</h1>
+            </li>
+            <li>
+                <i class="bi bi-sun-fill" id="icon" @click="toggle()"></i>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<style scoped lang="scss">
+    @import '../../public/Navbar.scss'
+</style>
