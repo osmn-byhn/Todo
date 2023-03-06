997dbbda72453b3408bc6f50f2267fe3465c7fa9
osmn-byhn <osmanbeyhan12@gmail.com>
2023-03-06 03:05:01 +0300

    Project Started

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <h1>NoteApp</h1>
+  </header>
+</template>
+
+<style scoped lang="scss">
+  
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+
+import './assets/main.scss'
+
+const app = createApp(App)
+
+app.use(createPinia())
+
+app.mount('#app')
