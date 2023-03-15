3598ceb566bfe47e084fdf0e809d9ad04ddbed65
osmn-byhn <osmanbeyhan12@gmail.com>
2023-03-16 01:01:11 +0300

    End Commit

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   import Navbar from './components/Navbar.vue';
   import { ref, onMounted, computed, reactive } from "vue";
   const todoText = ref('')
-  const deneme = ref([])
   const todo = {
     id: String,
     text: String,
@@ -34,10 +33,15 @@
       }
     }
     updateLocalStorage()
-    console.log(todoArray.value.filter((todo) => todo.isCompleted === true).length);
-    
   })
 
+  function toggleTodo(id) {
+    todoArray.value.forEach((todo) => {
+      if(todo.id === id ) todo.isCompleted = !todo.isCompleted
+    })
+    updateLocalStorage()
+  }
+  
   function addTodo() {
     todoArray.value = [
       ...todoArray.value,
@@ -50,34 +54,68 @@
     updateLocalStorage()
     todoText.value = ''
   }
+
+  function select() {
+    
+    if (todoBoolean === true) {
+      console.log("true");
+      todoBoolean.value = false
+    }
+    else {
+      console.log("false");
+      todoBoolean.value = true
+    }
+  }
   
-
-  
-
-
+  function clearCompletedTodos() {
+    todoArray.value = todoArray.value.filter((todo) => todo.isCompleted === false)
+    updateLocalStorage()
+  }
 </script>
 
 <template>
   <Navbar />
   <div class="main">
+    <div class="buttons">
+      <ul>
+        <li >
+          <span @click="clearCompletedTodos()" class="delete">Clear completed todos</span>
+        </li>
+        <li>
+          <span class="text">{{ todoArray.filter((todo) => todo.isCompleted === true).length }} out of {{ todoArray.length }} completed</span>
+        </li>
+      </ul>
+    </div>
     <progress
         :max="todoArray.length"
         :value="todoArray.filter((todo) => todo.isCompleted === true).length"
+        class="progressBar"
     ></progress>
-    <div>Clear completed todos</div>
-    <hr>
     <div class="form">
       <form>
-        <input type="text" name="" id="todoText" v-model="todoText">
-        <input type="submit" @click="addTodo()" value="Add" :disabled="todoText === ''" >
+        <input type="text" name="" class="todo-text" v-model="todoText">
+        <input type="submit" @click="addTodo()" value="Add" :disabled="todoText === ''" class="button">
       </form>
     </div>
   </div>
-  
-  <div v-for="(item, index) in todoArray" >
-    <p>{{ item.text }}</p>
+  <div class="todo-list" v-for="(item, index) in todoArray" >
+    <ul>
+      <li>
+        <input 
+          type="checkbox" 
+          name="isCompleted" 
+          :id="item.id" 
+          :checked="item.isCompleted"
+          @change="toggleTodo(item.id)"
+        />
+      </li>
+      <li>
+        <label :for="item.id" :class="item.isCompleted ? 'completed' : 'incomplete'">{{ item.text }}</label>
+      </li>
+    </ul>
   </div>
 </template>
 
 <style scoped lang="scss">
+  @import '../public/App.scss'
 </style>
